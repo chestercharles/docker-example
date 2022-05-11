2b7f8d49da2ec11a66ddd395e8f7d4430c9ce3e3
chestercharles <chestercarmer@icloud.com>
2022-05-11 09:14:47 -0700

    add comments

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const fetch = require("isomorphic-fetch");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 async function fetchPic() {
   const response = await fetch(
